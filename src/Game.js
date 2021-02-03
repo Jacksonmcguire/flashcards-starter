c2d7454614009e54e6350c6897064643dfb0c257
Jackson McGuire <jaxmcguire@gmail.com>
2021-02-03 09:53:52 -0500

    Pass game tests

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,33 @@
+const Card = require('./Card');
 const data = require('./data');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = new Round();
+  }
+
+  start() {
+    let cards = [];
+    prototypeQuestions.forEach(flashcard => {
+      const newCard = new Card
+      (flashcard.id, flashcard.question, 
+        flashcard.answers, flashcard.correctAnswer);
+      cards.push(newCard);
+    })
+    this.currentRound = new Round(cards);
+    return this.currentRound;
+  }
 
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
